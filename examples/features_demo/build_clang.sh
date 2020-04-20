ce488901c7a7443d11e24f64e624fccc67e5ec9f
ryanfleury <ryan.j.fleury@gmail.com>
2020-04-20 11:46:55 -0600

    Add no-write-strings flag to GCC/Clang scripts

--- examples/features_demo/build_clang.sh
+++ examples/features_demo/build_clang.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-clang -Wall -g -fPIC -shared -I ../../source/ ./features_demo.c -o features_demo.so
+clang -Wall -Wno-write-strings -g -fPIC -shared -I ../../source/ ./features_demo.c -o features_demo.so
 ../../build/data_desk --custom ./features_demo.so ./data.ds
 
